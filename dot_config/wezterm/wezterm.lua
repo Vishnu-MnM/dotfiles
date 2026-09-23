a5179762a9920661e55af1e9943dca781f203bc5
Vishnu_MnM <sunny_4210@proton.me>
2026-09-23 11:12:32 -0400

    Added wezterm config

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+local config = {}
+--Look and Feel
+config.font = wezterm.font 'JetBrains Mono Nerd Font Mono'
+config.font_size = 14.0
+config.color_scheme = "Noctalia"
+config.enable_tab_bar = false
+return config
